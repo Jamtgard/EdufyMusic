052e5b1337248e32aa01dec6e11dbbaaa6327b0e
jamtgard <jamtgardsimon@gmail.com>
2025-10-28 08:15:52 +0100

    Initial commit

---
+++ src/test/java/com/example/EdufyMusic/EdufyMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EdufyMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
