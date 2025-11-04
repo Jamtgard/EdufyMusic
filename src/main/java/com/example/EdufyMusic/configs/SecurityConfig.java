0e42a52a638ab340cbbcd7db45c5b6f34db7c9d5
jamtgard <jamtgardsimon@gmail.com>
2025-11-04 12:38:49 +0100

    ED-261-SJ

--- src/main/java/com/example/EdufyMusic/configs/SecurityConfig.java
+++ src/main/java/com/example/EdufyMusic/configs/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // ED-39-SJ
@@ -11,8 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
+/*
     // ED-39-SJ
+    // Activate later for PROD
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -28,7 +32,29 @@ public class SecurityConfig {
                         oauth2
                                 .jwt(jwt -> jwt.jwtAuthenticationConverter())
                 );
-         */
+          //
         return http.build();
+    }
+ */
+
+    // DEV for testing endpoints through Postman (ED-261-SJ)
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/music/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(basic -> {});
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("user").password("{noop}user").roles("USER").build();
+        var admin = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
