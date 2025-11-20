0252c5b3a74c49334cca85252360e22d1277b92d
jamtgard <jamtgardsimon@gmail.com>
2025-11-20 15:50:41 +0100

    ED-51-SJ * updated CommonController & added endpoint * added Request to CreatorClient. * Created MusicByCreatorDTO & MusicResponseDTO To handle both Song- & AlbumResponseDTO. * Created joined Service-layer for both Music-entities.

--- src/main/java/com/example/EdufyMusic/controllers/CommonController.java
+++ src/main/java/com/example/EdufyMusic/controllers/CommonController.java
@@ -1,12 +1,15 @@
 package com.example.EdufyMusic.controllers;
 
 import com.example.EdufyMusic.models.DTO.AlbumResponseDTO;
+import com.example.EdufyMusic.models.DTO.MusicResponseDTO;
 import com.example.EdufyMusic.models.DTO.SongResponseDTO;
 import com.example.EdufyMusic.services.AlbumService;
+import com.example.EdufyMusic.services.MusicService;
 import com.example.EdufyMusic.services.SongService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +22,13 @@ public class CommonController {
 
     private final SongService songService;
     private final AlbumService albumService;
+    private final MusicService musicService;
 
-    public CommonController(SongService songService, AlbumService albumService)
+    public CommonController(SongService songService, AlbumService albumService, MusicService musicService)
     {
         this.songService = songService;
         this.albumService = albumService;
+        this.musicService = musicService;
     }
 
     // ED-80-SJ
@@ -37,4 +42,11 @@ public class CommonController {
     public ResponseEntity<List<AlbumResponseDTO>> getAllAlbums(Authentication authentication) {
         return ResponseEntity.ok(albumService.getAllAlbums(authentication));
     }
+
+    // ED-51-SJ
+    @GetMapping("/discography/{creatorId}")
+    public ResponseEntity<MusicResponseDTO> getDiscography(@PathVariable Long creatorId, Authentication authentication){
+        return ResponseEntity.ok(musicService.getDiscography(creatorId, authentication));
+    }
+
 }
